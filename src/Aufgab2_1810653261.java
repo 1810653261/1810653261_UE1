42b9b54d4b8e91f0795f1877d6d4f34e4fd7f7c7
1810653261 <1810653261@stud.fh-kufstein.ac.at>
2018-11-17 19:16:12 +0100

    Aufgabe 2 geschrieben und auskommentiert

---
+++ src/Aufgab2_1810653261.java
@@ -0,0 +1,37 @@
+public class Aufgab2_1810653261 {
+
+    public static void main(String[] args) {
+        String name=("Alex"); // Der Datentyp String hat als Wertebereich Buchstaben
+        System.out.println("" + name); // Ausgabe des Datentyps string mit dem Wert "Alex"
+
+        int alter=(18);// Der Datentyp int hat als Wertebereich -2147483648 bis 2147483647
+        System.out.println("" + alter); // Ausgabe des Datentyps int mit dem Wert 18
+
+        double Kommazahl=(0.3333); // Der Datentyp double hat als Wertebereich -1.7 bis 1.7
+        System.out.println("" + Kommazahl); // Ausgabe des Datentyps double mit dem Wert 0.3333
+
+        boolean Wahrheit=false; // Der Datentyp boolean hat als Wertebereich true oder false
+        System.out.println("" + Wahrheit); // Ausgabe des Datentyps boolean mit dem Wert false
+
+        long Wert=(10); // Der Datentyp long hat als Wertebereich -9223372036854775808 bis 9223372036854775807
+        System.out.println("" + Wert); // Ausgabe des Datentyps long mit dem Wert 10
+
+        char Wertebereich='x'; // Der Datentyp char hat als Wertebereich Zeichen(Unicodes)
+        System.out.println("" + Wertebereich); // Ausgabe des Datentyps char mit dem wert x
+
+        byte Zahl=(120); // Der Datentyp byte hat als Wertebereich -128 bis 127
+        System.out.println("" + Zahl); // Ausgabe des Datentyps byte mit dem Wert 120
+
+        short variable=(240); // Der Datentyp short hat als Wertebereich -32768 bis 32767
+        System.out.println("" + variable); // Ausgabe des Datentyps short mit dem Wert 240
+
+        float fließKommazahlen=(3); // Der Datentyp float hat als Wertebereich Fließkommazahlen
+                                    //mit einfacher Genauigkeit
+        System.out.println("" + fließKommazahlen); // Ausgabe des Datentyps float mit dem Wert 3
+
+        int zahl=(218); // Eine Variable hat einen unbestimmten Wert, eine Konstanten hat einen festen Zugewisenen Wert
+        System.out.println("" + zahl);
+
+
+    }
+}
