99bac93ff15f7a4ce15c1652425e57ceeb76a0f9
1810653261 <1810653261@stud.fh-kufstein.ac.at>
2018-11-19 13:11:39 +0100

    Aufgabe 3 rechenaufgabe ausgeführt und auskommentiert

---
+++ src/Aufgabe3_1810653261.java
@@ -0,0 +1,24 @@
+public class Aufgabe3_1810653261 {
+
+    public static void main(String[]args) {
+        int zahl1=(15);
+        int zahl2=(10);
+        int zahl3=(5);
+
+        double zahl4=(0.3);
+        double zahl5=(0.4);
+        double zahl6=(0.6);
+
+        System.out.println(zahl1 + zahl2); // zahl 1 plus zahl 2
+
+        System.out.println(zahl1 / zahl3); // zahl 1 geteilt durch zahl 3
+
+        System.out.println(zahl4 * zahl2); // zahl 3 mal zahl 2
+
+        System.out.println(zahl1 % zahl2); // zahl 1 modulo zahl 2
+
+        System.out.println(++zahl1); // Zahl 1 inkrementiert
+
+        System.out.println(--zahl2); // Zahl 2 dekrementiert
+    }
+}
