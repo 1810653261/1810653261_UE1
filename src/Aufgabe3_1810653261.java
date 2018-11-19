38f6e592aed340d2e1c95f36241c867883041e6f
1810653261 <1810653261@stud.fh-kufstein.ac.at>
2018-11-19 18:48:39 +0100

    Aufgabe 4 fertiggestellt

--- src/Aufgabe3_1810653261.java
+++ src/Aufgabe3_1810653261.java
@@ -1,13 +1,13 @@
 public class Aufgabe3_1810653261 {
 
-    public static void main(String[]args) {
-        int zahl1=(15);
-        int zahl2=(10);
-        int zahl3=(5);
+    public static void main(String[]args) { //main Methode erstellt
+        int zahl1=(15); // variable zahl1 mit wert 15 erstellt
+        int zahl2=(10); // variable zahl2 mit wert 10 erstellt
+        int zahl3=(5); // variable zahl3 mit wert 5 erstellt
 
-        double zahl4=(0.3);
-        double zahl5=(0.4);
-        double zahl6=(0.6);
+        double zahl4=(0.3); // variable zahl4 mit wert o.3 erstellt
+        double zahl5=(0.4); // variable zahl5 mit wert 0.4 erstellt
+        double zahl6=(0.6); // variable zahl6 mit dem wert 0.6 erstellt
 
         System.out.println(zahl1 + zahl2); // zahl 1 plus zahl 2
 
@@ -17,7 +17,7 @@ public class Aufgabe3_1810653261 {
 
         System.out.println(zahl1 % zahl2); // zahl 1 modulo zahl 2
 
-        System.out.println(++zahl1); // Zahl 1 inkrementiert
+        System.out.println(++zahl3); // Zahl 3 inkrementiert
 
         System.out.println(--zahl2); // Zahl 2 dekrementiert
     }
--- src/Aufgabe4_1810653261.java
+++ src/Aufgabe4_1810653261.java
@@ -1,2 +1,25 @@
 public class Aufgabe4_1810653261 {
+    public static void main(String[]args) {
+
+    int zahl1=(15);
+    int zahl2=(10);
+
+
+    System.out.println(zahl1 < zahl2);
+    System.out.println(zahl2 < zahl1);
+    System.out.println(zahl1 > zahl2);
+    System.out.println(zahl2 > zahl1);
+    System.out.println(zahl1 <= zahl2);
+    System.out.println(zahl2 <= zahl1);
+    System.out.println(zahl1 >= zahl2);
+    System.out.println(zahl2 >= zahl1);
+    System.out.println(zahl1 == zahl2);
+    System.out.println(zahl1 != zahl2);
+
+
+
+
+
+
+    }
 }
