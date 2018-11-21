4e20939f170536ead00e762da798cd0de9999216
1810653261 <1810653261@stud.fh-kufstein.ac.at>
2018-11-21 18:45:57 +0100

    Aufgabe 5 fertig geschrieben und auskommentiert

---
+++ src/Aufgabe5_1810653261.java
@@ -0,0 +1,20 @@
+public class Aufgabe5_1810653261 {
+
+
+    public static void main(String[]args) {
+
+        int zahl1=(10);
+        int zahl2=(20);
+
+        System.out.println(zahl1++); //bei zahl1++ wird die zahl nach der ausgabe um eins erhöht, bei ++zahl1 wird vor der ausgabe die zahl um eins erhöht.
+        System.out.println(zahl2++);
+
+        System.out.println(zahl1);
+        System.out.println(zahl2);
+
+        System.out.println(++zahl1);
+        System.out.println(++zahl2);
+
+    }
+
+}
